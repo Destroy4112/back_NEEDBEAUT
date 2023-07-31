d7cc13b6a8a9c6b7e9114c482ca6f286444e12a4
Juan Arrieta <juanfearrietaortiz@gmail.com>
2023-07-31 18:43:15 -0500

    login y registro devuelve usuario

--- app/Http/Controllers/tiendasController.php
+++ app/Http/Controllers/tiendasController.php
@@ -63,7 +63,7 @@ class tiendasController extends Controller
             $Image->save();
         }
 
-        return response()->json(['message' => 'Tienda almacenada correctamente'], 201);
+        return response()->json(['data' => $tienda], 201);
     }
 
 
@@ -79,19 +79,19 @@ class tiendasController extends Controller
             if (Hash::check($request->password, $tienda->password)) {
                 return response()->json([
                     "status" => 1,
-                    "message" => "usuario ingresado correctamente",
+                    "data" => $tienda
                 ]);
             } else {
                 return response()->json([
-                    "status" => 0,
+                    "status" => 2,
                     "message" => "password incorrecta",
-                ], 404);
+                ], 200);
             }
         } else {
             return response()->json([
                 "status" => 0,
                 "message" => "Usuario no Registrado",
-            ], 404);
+            ], 200);
         }
     }
 
