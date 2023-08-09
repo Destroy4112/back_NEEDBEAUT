13e9436923d3d8229873032d29ace795f0b3d923
BrayanMontes <brayanjose199907@gmail.com>
2023-08-09 18:32:46 -0500

    nuevo

--- app/Models/products.php
+++ app/Models/products.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class products extends Model
 {
     use HasFactory;
-
+protected $guarded=[];
     public function tienda()
     {
         return $this->belongsTo(tiendas::class, 'tienda_id');
