a5698d236b6ae93f2fe773bfe0d98b84583671e8
BrayanMontes <brayanjose199907@gmail.com>
2023-08-09 20:30:18 -0500

    rutas de metodos add perfil y portada agregados

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\tiendasController;
-
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\categoriasController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,9 +19,14 @@ use App\Http\Controllers\tiendasController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::get('tiendas', [tiendasController::class, 'index']);
 Route::post('tiendas', [tiendasController::class, 'store']);
 Route::post('tiendas/login', [tiendasController::class, 'login']);
 Route::put('tiendas/{id}', [tiendasController::class, 'update']);
 Route::delete('tiendas/{tienda}', [tiendasController::class, 'destroy']);
 Route::get('tiendas/{tienda}', [tiendasController::class, 'show']);
+Route::get('tiendas/{categoria_id}', [tiendasController::class, 'category']);
+
+Route::post('tiendas/{tienda}/imagenPerfil',[tiendasController::class, 'imagenPerfil']);
+Route::post('tiendas/{tienda}/imagenPortada',[tiendasController::class, 'imagenPortada']);
