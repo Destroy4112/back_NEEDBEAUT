bfe1215f0a57fe8004d41297e92e45b41d23b4d2
BrayanMontes <brayanjose199907@gmail.com>
2023-08-17 20:30:13 -0500

    ruta actualizar datos de tienda, creado

--- routes/api.php
+++ routes/api.php
@@ -23,16 +23,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('tiendas', [tiendasController::class, 'index']);
 Route::post('tiendas', [tiendasController::class, 'store']);
 Route::post('tiendas/login', [tiendasController::class, 'login']);
-Route::put('tiendas/{id}', [tiendasController::class, 'update']);
+Route::put('tiendas/{id}/update-tienda', [tiendasController::class, 'updateTienda']);
 Route::delete('tiendas/{tienda}', [tiendasController::class, 'destroy']);
 Route::get('tiendas/{tienda}', [tiendasController::class, 'show']);
 Route::get('tiendas/{categoria_id}', [tiendasController::class, 'category']);
 Route::post('tiendas/{tienda}/imagenPerfil',[tiendasController::class, 'imagenPerfil']);
 Route::post('tiendas/{tienda}/imagenPortada',[tiendasController::class, 'imagenPortada']);
 Route::get('tiendas/categoria/{categoria}',[tiendasController::class, 'tiendasPorCategoria']);
-Route::patch('tiendas/{tienda}/updateimagenperfil',[tiendasController::class, 'updateimagenPerfil']);
 Route::post('tiendas/{tienda_id}/add-destacadas', [tiendasController::Class, 'addDestacadas']);
-
+Route::put('tiendas/{id}/actualizarImagen',[tiendasController::Class,'actualizarImagen']);
 
 Route::post('products', [ProductsController::class, 'store']);
 Route::get('productos-por-tienda/{tiendaId}', [tiendasController::class, 'mostrarProductosPorTienda']);
