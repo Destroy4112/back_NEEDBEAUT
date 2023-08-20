2b64ef122ba6c7561250bf842cd48ca5525516bd
BrayanMontes <brayanjose199907@gmail.com>
2023-08-20 18:29:05 -0500

    campo slogan creado

--- database/migrations/2023_07_28_034030_create_tiendas_table.php
+++ database/migrations/2023_07_28_034030_create_tiendas_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('negocio');
+            $table->string('slogan');
             $table->string('categoria');
             $table->string('nit')->unique();
             $table->string('ubicacion');
