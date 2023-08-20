fd1bbb6c0f8b99829266a1eff7d532cff3c8ea79
BrayanMontes <brayanjose199907@gmail.com>
2023-08-20 18:28:21 -0500

    metodo mostrar tiendas por productos creado

--- app/Http/Controllers/productsController.php
+++ app/Http/Controllers/productsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\products;
 use App\Models\tiendas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 
 
@@ -25,22 +26,80 @@ class productsController extends Controller
             'nombre' => 'required|string',
             'precio' => 'required|min:3',
             'cantidad' => 'required|integer|min:1',
+            'imagen' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
+
          // Crear el registro de los productos en la base de datos
-         $products = products::create($request->all());
+         $products= new products([
+            'tienda_id' => $request->tienda_id,
+            'codigo' => $request->codigo,
+            'nombre' => $request->nombre,
+            'precio'=>$request->precio,
+            'cantidad' => $request->cantidad,
+            'imagen'=>$request->imagen,
+            
+        ]);
+         //$products = products::create($request->all());
+         $imagen= $request->file('imagen')->store('public/images');
+         $imagenUrl = Storage::url($imagen);
+         $products->imagen = $imagenUrl;
+         $products->save();
          return response()->json(['message' =>' producto guardado exitosamente'], 201);
 
     }
 
     public function mostrarTiendaPorProducto($nombre)
     {
-        $tienda = Products::where('nombre', $nombre)->get();
-        $tiendas = $tienda->map(function ($producto) {
-          
-            return Tiendas::find($producto->tienda_id);
-     
+        $producto = Products::where('nombre', $nombre)->get();
+        if(!$producto){
+            return response()->json(['error'=>'producto no encontrado'], 404);
+        }
+        $tiendasconProductos = Tiendas::whereHas('products', function ($query) use ($nombre) {
+            $query->where('nombre', $nombre);
+        })->get();
+        $tiendasInfo = $tiendasconProductos->map(function ($tienda) {
+            return [
+                'nit' => $tienda->nit,
+                'negocio' => $tienda->negocio,
+                'ubicacion' => $tienda->ubicacion,
+                'telefono' => $tienda->telefono,
+                'email'=> $tienda->email,
+            ];
         });
-        return response()->json($tiendas);
+        return response()->json($tiendasInfo);
     }
+  //no funciona
+    public function updateProducto(Request $request, $id)
+{
+    $request->validate([
+      
+        'codigo' => 'required|string',
+        'nombre' => 'required|string',
+        'precio' => 'required|min:3',
+        'cantidad' => 'required|integer|min:1',
+        'imagen' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+    ]);
 
+    $product = Products::findOrFail($id);
+
+    // Actualizar los campos del producto
+    
+    $product->codigo = $request->codigo;
+    $product->nombre = $request->nombre;
+    $product->precio = $request->precio;
+    $product->cantidad = $request->cantidad;
+
+    if ($request->hasFile('imagen')) {
+        // Eliminar la imagen anterior si existe
+        Storage::delete($product->imagen);
+
+        // Subir la nueva imagen
+        $imagen = $request->file('imagen')->store('public/images');
+        $imagenUrl = Storage::url($imagen);
+        $product->imagen = $imagenUrl;
+    }
+    $product->save();
+    return response()->json(['message' => 'Producto actualizado exitosamente'], 200);
+}
+ 
 }
